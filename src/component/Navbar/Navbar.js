2c65679d51bf874baa782a2536d4559e94ca10b2
sauravsuthar <sauravv.works@gmail.com>
2024-08-23 08:22:40 +0530

    react code

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assegts/logo.png'
 import cart_icon from '../Assegts/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 export const Navbar = () => {
   const [menu , setMenu] = useState("shop")
+  const {getTotalCartItems} = useContext(ShopContext);
   return (
     <div className='navbar'>
       <div  className='nav-logo'>
@@ -30,7 +32,7 @@ export const Navbar = () => {
         
         
        
-        <div className='nav-cart-count'>0</div>
+        <div className='nav-cart-count'>{getTotalCartItems()}</div>
 
       </div>
         
--- src/component/ProductDisplay/ProductDisplay.js
+++ src/component/ProductDisplay/ProductDisplay.js
@@ -6,9 +6,9 @@ import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
-   const {addToCart} = useContext(ShopContext)
+  const { addToCart } = useContext(ShopContext);
   return (
-    <div className="productDisplay">
+    <div className="productdisplay">
       <div className="productdisplay-left">
         <div className="productdisplay-img-list">
           <img src={product.image} alt="" />
@@ -16,13 +16,13 @@ const ProductDisplay = (props) => {
           <img src={product.image} alt="" />
           <img src={product.image} alt="" />
         </div>
-        <div className="productdisplay-image">
+        <div className="productdisplay-img">
           <img className="productdisplay-main-img" src={product.image} alt="" />
         </div>
       </div>
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
-        <div className="productdisplay-right-starts">
+        <div className="productdisplay-right-stars">
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
@@ -38,30 +38,38 @@ const ProductDisplay = (props) => {
             ${product.new_price}
           </div>
         </div>
-        <div className="productdisplay-right-descryption">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
-          maxime.
+        <div className="productdisplay-right-description">
+          A lightweight, usually knitted, pullover shirt, close-fitting and with
+          a round neckline and short sleeves, worn as an undershirt or outer
+          garment.
         </div>
         <div className="productdisplay-right-size">
-          <h1>select size</h1>
+          <h1>Select Size</h1>
           <div className="productdisplay-right-sizes">
             <div>S</div>
-            <div>m</div>
-            <div>l</div>
-            <div>Xl</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
             <div>XXL</div>
           </div>
-          <button onClick={()=>{addToCart(product.Id)}}>add to cart</button>
-          <p className="productdisplay-right-category">
-            <span>Category:</span>Women,T-shirt
-          </p>
-          <p className="productdisplay-right-category">
-            <span>Tags:</span>Morderen, latest 
-          </p>
         </div>
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          ADD TO CART
+        </button>
+        <p className="productdisplay-right-category">
+          <span>Category : </span>
+          Women, T-shirt, Crop Top
+        </p>
+        <p className="productdisplay-right-category">
+          <span>Tags : </span>
+          Modern, Latest
+        </p>
       </div>
     </div>
   );
 };
-
 export default ProductDisplay;
